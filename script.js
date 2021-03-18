2df65f8fc47031e4dbf343b71f9ef9495cb2e9a0
usama razzaq <usamarazzaq2525@gmail.com>
2021-03-18 13:45:30 +0500

    Done

---
+++ script.js
@@ -0,0 +1,127 @@
+// var location = window.location.href;
+// console.log(location) 
+// var location = window.location.hostname;
+// console.log(location) 
+
+// Form Validation
+// function checkNAme()
+// {
+//     var tag = document.getElementById("name")
+//     var nameLeng = document.getElementById("name").value;
+//     if(nameLeng.length == 0)
+//     {
+//         alert("Name field is required")
+//         // document.getElementById("name").focus();
+//         tag.focus()
+//         tag.style.backgroundColor = "red"
+//     }
+// }
+
+// function checkForSelection()
+// {
+//  if (document.getElementById("states").selectedIndex === 0) 
+//   {
+//     alert("Please select a state.");
+//      return false;
+//     }
+// }
+
+// function checkForSelection(states) 
+// {
+//      var target = document.getElementById("states");
+//      if (target.selectedIndex === 0) 
+//      {
+//      alert("Please select a state.");
+//      return false;
+//      }
+// }
+
+//  function validateRadios() {
+//      var radios = document.getElementsByName("r1");
+//      for (var i = 0; i < radios.length; i++) {
+//      if (radios[i].checked) {
+//      return true;
+//     }
+//     }
+//      alert("Please check one.");
+//      return false;
+//      }
+
+
+// function validatingEmail()
+// {
+//     var val =document.getElementById("e").value;
+//     var sli
+//     for(i=0; i<=val.length; i++)
+//     {
+//         sli = val.slice(i,i+1)
+//         if(sli == " ")
+//         {
+//             alert("Spaces are not allowed in email field.")
+//         }
+//     }
+    
+// }
+// function validatingEmail()
+// {
+//     var val =document.getElementById("e").value;
+//     var counter= 0 
+//     var sli
+//     for(i=0; i<=val.length; i++)
+//     {
+//         sli = val.slice(i,i+1)
+//         if(sli == "@")
+//         {
+//             counter = 1;
+//         }
+//     }
+//     if(counter== 0 )
+//     {
+//         alert("Email must include an @ sign")
+//     }
+// }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
